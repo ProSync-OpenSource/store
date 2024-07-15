1b66fc6c13003bbd0ccd536acc1caf706c320478
brendoarruda <brendo.arruda@gmail.com>
2024-07-14 21:38:44 -0300

    feat

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,19 +1,20 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
 
 export default [
   js.configs.recommended,
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
+  { extends: ['prettier'] },
   {
     rules: {
-      "react/react-in-jsx-scope": "off",
-    },
-  },
-];
+      'react/react-in-jsx-scope': 'off'
+    }
+  }
+]
---
+++ frontend/src/components/Main/index.tsx
@@ -0,0 +1,6 @@
+'use client'
+import * as S from './styles'
+
+export default function Main() {
+  return <S.Container>testando</S.Container>
+}
