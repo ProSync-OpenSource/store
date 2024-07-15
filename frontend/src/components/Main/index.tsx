6bf3bb6c5a7fdef5a70edef19c7c2f3503aaef83
brendoarruda <brendo.arruda@gmail.com>
2024-07-14 22:12:34 -0300

    feat

--- frontend/src/components/Main/index.tsx
+++ frontend/src/components/Main/index.tsx
@@ -4,7 +4,7 @@ import * as S from './styles'
 export default function Main() {
   return (
     <S.Container>
-      <p className="text-5xl">testando</p>
+      <p className="text-5xl btn btn-primary">testando</p>
     </S.Container>
   )
 }
