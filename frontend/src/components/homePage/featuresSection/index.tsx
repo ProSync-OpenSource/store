4c1d7ceecfa9974101586cc3b679f854fd15b36b
Vitor Hugo <Hugosilva55442@gmail.com>
2024-08-11 11:21:02 -0300

    feat: add section title to component

--- frontend/src/components/homePage/featuresSection/index.tsx
+++ frontend/src/components/homePage/featuresSection/index.tsx
@@ -1,4 +1,6 @@
-import { FeaturesContainer } from "@/components/homePage/styles";
+import { useState, useRef, useEffect, MutableRefObject } from "react";
+
+import { FeaturesContainer, ProductsSectionTitleBox } from "@/components/homePage/styles";
 
 import Family_img from '@@/public/assets/home/family_img.jpg';
 import Kitchen_img from '@@/public/assets/home/kitchen_img.jpg';
@@ -7,70 +9,79 @@ import Woman_model_2 from '@@/public/assets/home/woman_model_2.jpg';
 
 import Image from "next/image";
 
-export function FeaturesSection() {
+export default function FeaturesSection() {
+    const [sectionTitleWidth, setSectionTitleWidth] = useState(0);
+    const sectionTitleRef: MutableRefObject<HTMLHeadingElement | null> = useRef(null)
+
+    useEffect(() => {
+        sectionTitleRef.current?.offsetWidth && 
+          setSectionTitleWidth(sectionTitleRef.current.offsetWidth);
+      },[sectionTitleRef]);
+
     return (
-        <FeaturesContainer>
-            <div className='min-h-full order-1 row-span-2 rounded-md overflow-hidden cursor-pointer relative'>
-                <Image
-                    src={Woman_model_2}
-                    alt={'black woman wearing a T-shirt and black glasses'}
-                    width={500}
-                    height={500}
-                    className='w-full h-auto object-cover max-[750px]:object-bottom'
-                />
+        <div className="px-10 max-[750px]:px-[15px]">
 
-                <div className="features-overlay">
-                    <h4 className="text-white font-semibold text-4xl max-[900px]:text-3xl max-[800px]:text-2xl">Estilo & Elegancia</h4>
-                    <button className="rounded-sm w-36 h-8 bg-primary text-white uppercase font-normal text-sm outline-none max-[900px]:text-[13px] max-[900px]:w-32 max-[800px]:w-28 max-[800px]:text-xs max-[800px]:font-medium">Confira</button>
-                </div>
-            </div>
+            <ProductsSectionTitleBox afterElementWidth={sectionTitleWidth}>
+                <h3 ref={sectionTitleRef} className='text-gray-900 relative font-semibold text-xl top-0 py-2 max-[900px]:text-lg max-[550px]:text-base'>Confira os nossos destaques</h3>
+                <p className='text-gray-900 font-semibold text-base leading-none max-[550px]:text-sm max-[550px]:hidden'>Ver todos</p>
+            </ProductsSectionTitleBox>
 
-            <div className='order-2 rounded-md overflow-hidden cursor-pointer relative'>
-                <Image
-                    src={Ball_img}
-                    alt={'yellow tennis ball'}
-                    width={500}
-                    height={500}
-                    className='w-full h-full object-cover'
-                />
-
-                <div className="features-overlay">
-                    <h4 className="text-white font-semibold text-3xl max-[900px]:text-3xl max-[800px]:text-2xl">Esportes & Lazer</h4>
-                    <button className="rounded-sm w-36 h-8 bg-primary text-white uppercase font-normal text-sm outline-none max-[900px]:text-[13px] max-[900px]:w-32 max-[800px]:w-28 max-[800px]:text-xs max-[800px]:font-medium">Confira</button>
-                </div>
-            </div>
-
-            <div className='order-3 grid grid-cols-2 gap-x-3'>
-                <div className='rounded-md overflow-hidden cursor-pointer relative'>
+            <FeaturesContainer>
+                <div className='min-h-full order-1 row-span-2 rounded-md overflow-hidden cursor-pointer relative'>
                     <Image
-                        src={Kitchen_img}
-                        alt={'kitchen furniture'}
+                        src={Woman_model_2}
+                        alt={'black woman wearing a T-shirt and black glasses'}
                         width={500}
                         height={500}
-                        className='w-full h-full object-cover'
+                        className='w-full h-auto object-cover max-[750px]:object-bottom'
                     />
-
                     <div className="features-overlay">
-                        <h4 className="text-white font-semibold text-2xl max-[900px]:text-xl max-[800px]:text-lg max-[700px]:text-sm">Eletrodomesticos</h4>
+                        <h4 className="text-white font-semibold text-4xl max-[900px]:text-2xl max-[800px]:text-2xl">Estilo & Elegancia</h4>
                         <button className="rounded-sm w-36 h-8 bg-primary text-white uppercase font-normal text-sm outline-none max-[900px]:text-[13px] max-[900px]:w-32 max-[800px]:w-28 max-[800px]:text-xs max-[800px]:font-medium">Confira</button>
                     </div>
                 </div>
-
                 <div className='order-2 rounded-md overflow-hidden cursor-pointer relative'>
                     <Image
-                        src={Family_img}
-                        alt={'Black family in the kitchen preparing breakfast'}
+                        src={Ball_img}
+                        alt={'yellow tennis ball'}
                         width={500}
                         height={500}
                         className='w-full h-full object-cover'
                     />
-
                     <div className="features-overlay">
-                        <h4 className="text-white font-semibold text-2xl max-[900px]:text-xl max-[800px]:text-lg max-[700px]:text-sm">Familia</h4>
+                        <h4 className="text-white font-semibold text-3xl max-[900px]:text-2xl max-[800px]:text-2xl">Esportes & Lazer</h4>
                         <button className="rounded-sm w-36 h-8 bg-primary text-white uppercase font-normal text-sm outline-none max-[900px]:text-[13px] max-[900px]:w-32 max-[800px]:w-28 max-[800px]:text-xs max-[800px]:font-medium">Confira</button>
                     </div>
                 </div>
-            </div>
-        </FeaturesContainer>
+                <div className='order-3 grid grid-cols-2 gap-x-3'>
+                    <div className='rounded-md overflow-hidden cursor-pointer relative'>
+                        <Image
+                            src={Kitchen_img}
+                            alt={'kitchen furniture'}
+                            width={500}
+                            height={500}
+                            className='w-full h-full object-cover'
+                        />
+                        <div className="features-overlay">
+                            <h4 className="text-white font-semibold text-2xl max-[900px]:text-base max-[800px]:text-lg max-[700px]:text-sm">Eletrodomesticos</h4>
+                            <button className="rounded-sm w-36 h-8 bg-primary text-white uppercase font-normal text-sm outline-none max-[900px]:text-[13px] max-[900px]:w-32 max-[800px]:w-28 max-[800px]:text-xs max-[800px]:font-medium">Confira</button>
+                        </div>
+                    </div>
+                    <div className='order-2 rounded-md overflow-hidden cursor-pointer relative'>
+                        <Image
+                            src={Family_img}
+                            alt={'Black family in the kitchen preparing breakfast'}
+                            width={500}
+                            height={500}
+                            className='w-full h-full object-cover'
+                        />
+                        <div className="features-overlay">
+                            <h4 className="text-white font-semibold text-2xl max-[900px]:text-base max-[800px]:text-lg max-[700px]:text-sm">Familia</h4>
+                            <button className="rounded-sm w-36 h-8 bg-primary text-white uppercase font-normal text-sm outline-none max-[900px]:text-[13px] max-[900px]:w-32 max-[800px]:w-28 max-[800px]:text-xs max-[800px]:font-medium">Confira</button>
+                        </div>
+                    </div>
+                </div>
+            </FeaturesContainer>
+        </div>
     );
 };
--- frontend/src/components/homePage/productsCarousel/modelOne/index.tsx
+++ frontend/src/components/homePage/productsCarousel/modelOne/index.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 
+import { useRef, MutableRefObject, useState, useEffect } from 'react';
+
 import { FaStar } from 'react-icons/fa'
 import { IoIosAdd } from "react-icons/io";
 
@@ -9,6 +11,8 @@ import Shipping_icon from '@@/public/assets/home/shipping_2.png'
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
+
+import { ProductsSectionTitleBox } from '@/components/homePage/styles';
 
 import '@@/node_modules/swiper/swiper.css';
 import '@@/node_modules/swiper/modules/pagination.css';
@@ -20,10 +24,14 @@ import { ProductsSlideElement, SwiperContainer } from '@/components/homePage/sty
 type SwiperProps = {
   productsData?: any;
   navigation?: { nextEl: string; prevEl: string };
-  width?: string | number;
+  title?: string;
 }
 
-export function ProductsModelOne(props: SwiperProps) {
+export default function ProductsModelOne(props: SwiperProps) {
+  const [sectionTitleWidth, setSectionTitleWidth] = useState(0);
+
+  const sectionTitleRef: MutableRefObject<HTMLHeadingElement | null> = useRef(null)
+
   const breakpoints = {
     300: {
       spaceBetween: 15
@@ -32,6 +40,11 @@ export function ProductsModelOne(props: SwiperProps) {
       spaceBetween: 20
     }
   };
+
+  useEffect(() => {
+    sectionTitleRef.current?.offsetWidth && 
+      setSectionTitleWidth(sectionTitleRef.current.offsetWidth);
+  },[sectionTitleRef]);
 
   const slideElement: JSX.Element =
     props.productsData &&
@@ -63,7 +76,7 @@ export function ProductsModelOne(props: SwiperProps) {
 
               <div className='flex items-center gap-x-0'>
                 <p className='text-gray-800 font-medium leading-none text-xs max-[900px]:text-[13px]'>{props.productsData.vote_average}</p>
-                <IoIosAdd className='text-black text-sm'/>
+                <IoIosAdd className='text-black text-sm' />
               </div>
             </div>
 
@@ -72,8 +85,8 @@ export function ProductsModelOne(props: SwiperProps) {
               <>
                 <p className='text-gray-500 text-sm font-medium mt-1 leading-tight line-through max-[900px]:text-[13px]'>R${props.productsData.product_price}</p>
                 <div className='flex items-center gap-x-2'>
-                   <p className='text-gray-800 font-semibold text-[27px] leading-tight'>R$ {`${(props.productsData.product_price * (1 - props.productsData.offer)).toFixed(2)}`.replace('.', ',')}</p>
-                    <p className='text-primary font-medium text-sm'>{props.productsData.offer * 100}% OFF</p>
+                  <p className='text-gray-800 font-semibold text-[27px] leading-tight max-[900px]:text-2xl'>R$ {`${(props.productsData.product_price * (1 - props.productsData.offer)).toFixed(2)}`.replace('.', ',')}</p>
+                  <p className='text-primary font-medium text-sm'>{props.productsData.offer * 100}% OFF</p>
                 </div>
               </>
 
@@ -82,9 +95,9 @@ export function ProductsModelOne(props: SwiperProps) {
             )}
 
             <p className='w-full text-gray-500 font-medium text-sm relative max-[900px]:text-[13px]'>
-                Até <span className='text-gray-950'>{props.productsData.installments}x de
+              Até <span className='text-gray-950'>{props.productsData.installments}x de
                 R$ {`${props.productsData.installments_price}`.replace('.', ',')}
-                </span>
+              </span>
             </p>
           </div>
 
@@ -104,70 +117,90 @@ export function ProductsModelOne(props: SwiperProps) {
 
   return (
     <SwiperContainer>
-      <div className='px-7'>
-        <Swiper
-          style={{
-            width: props.width? props.width : 'auto',
-            height: 420,
-            zIndex: 5,
-            overflow: 'hidden',
-            boxSizing: 'border-box',
-            padding: '30px 0px',
-          }}
-          slidesPerView={'auto'}
-          slidesPerGroupAuto={true}
-          loop={false}
-          navigation={{
-            nextEl: `.${props.navigation?.nextEl}`,
-            prevEl: `.${props.navigation?.prevEl}`,
-          }}
-          modules={[Navigation]}
-          breakpoints={breakpoints}
-        >
-          {slideElement}
-          {slideElement}
-          {slideElement}
-          {slideElement}
-          {slideElement}
-          {slideElement}
-          {slideElement}
-          {slideElement}
-          {slideElement}
-          {slideElement}
-          {slideElement}
-          {slideElement}
-          {slideElement}
-          {slideElement}
-          {slideElement}
-          {slideElement}
-          {slideElement}
-          {slideElement}
-          {slideElement}
-          {slideElement}
-          {slideElement}
-          {slideElement}
-          {slideElement}
-          {slideElement}
-        </Swiper>
-        <div className='absolute top-1/2 -translate-y-1/2 z-30 swiper-controllers w-full max-[800px]:hidden max-w-screen box-border'>
-          <button className={`${props.navigation?.prevEl} absolute z-10 left-0 top-2/4 -translate-y-2/4 outline-none -translate-x-2/4 rounded-full w-14 h-14 flex justify-center items-center bg-base-color max-[900px]:w-12 max-[900px]:h-12`}>
-            <Image
-              src={Arrow_left}
-              alt={'arrow left picture'}
-              width={500}
-              height={500}
-              className='w-6 h-auto object-cover max-[900px]:w-5'
-            />
-          </button>
-          <button className={`${props.navigation?.nextEl} absolute top-2/4 z-20 -translate-y-2/4 outline-none right-14 translate-x-2/4 rounded-full bg-base-color w-14 h-14 flex justify-center items-center max-[900px]:w-12 max-[900px]:h-12`}>
-            <Image
-              src={Arrow_right}
-              alt={'arrow left picture'}
-              width={500}
-              height={500}
-              className='w-6 h-auto object-cover max-[900px]:w-5'
-            />
-          </button>
+      <div className='flex flex-col items-center justify-center max-[750px]:px-0'>
+
+        <div className='w-full px-7 max-[750px]:px-[15px]'>
+          {props.title ? (
+            <ProductsSectionTitleBox afterElementWidth={sectionTitleWidth}>
+              <h3 ref={sectionTitleRef} className='text-gray-900 relative font-semibold text-xl py-2 max-[900px]:text-lg max-[550px]:text-base'>{props.title}</h3>
+              <p className='text-gray-900 font-semibold text-base leading-none max-[550px]:text-sm max-[550px]:hidden'>Ver todos</p>
+            </ProductsSectionTitleBox>
+          ) : null}
+        </div>
+
+        <div className='px-7 w-full max-[750px]:px-0'>
+          <div className='w-full relative overflow-x-visible'>
+            <Swiper
+              style={{
+                width: '100%',
+                height: 410,
+                zIndex: 5,
+                overflow: 'hidden',
+                boxSizing: 'border-box',
+                padding: '20px 6px 25px 6px',
+              }}
+
+              className='products-swiper'
+
+              slidesPerView={'auto'}
+
+              slidesPerGroupAuto={true}
+
+              loop={false}
+
+              navigation={{
+                nextEl: `.${props.navigation?.nextEl}`,
+                prevEl: `.${props.navigation?.prevEl}`,
+              }}
+              modules={[Navigation]}
+              breakpoints={breakpoints}
+            >
+              {slideElement}
+              {slideElement}
+              {slideElement}
+              {slideElement}
+              {slideElement}
+              {slideElement}
+              {slideElement}
+              {slideElement}
+              {slideElement}
+              {slideElement}
+              {slideElement}
+              {slideElement}
+              {slideElement}
+              {slideElement}
+              {slideElement}
+              {slideElement}
+              {slideElement}
+              {slideElement}
+              {slideElement}
+              {slideElement}
+              {slideElement}
+              {slideElement}
+              {slideElement}
+              {slideElement}
+            </Swiper>
+            <div className='absolute top-1/2 left-0 -translate-y-1/2 z-30 swiper-controllers w-full max-[800px]:hidden max-w-screen box-border'>
+              <button className={`${props.navigation?.prevEl} absolute z-10 left-0 top-2/4 -translate-y-2/4 -translate-x-1/2 outline-none rounded-full w-14 h-14 flex justify-center items-center bg-base-color max-[900px]:w-12 max-[900px]:h-12`}>
+                <Image
+                  src={Arrow_left}
+                  alt={'arrow left picture'}
+                  width={500}
+                  height={500}
+                  className='w-6 h-auto object-cover max-[900px]:w-5'
+                />
+              </button>
+              <button className={`${props.navigation?.nextEl} absolute top-2/4 z-20 -translate-y-2/4  translate-x-1/2 outline-none right-0 rounded-full bg-base-color w-14 h-14 flex justify-center items-center max-[900px]:w-12 max-[900px]:h-12`}>
+                <Image
+                  src={Arrow_right}
+                  alt={'arrow left picture'}
+                  width={500}
+                  height={500}
+                  className='w-6 h-auto object-cover max-[900px]:w-5'
+                />
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </SwiperContainer>
